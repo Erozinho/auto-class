45f720b5ffc5dc71a22785f7fab04508a7397406
Felipe Araujo <zenycsoficial@gmail.com>
2023-07-26 10:04:21 -0300

    improve AULA 10

--- AULA 10 - EX using News API.py
+++ AULA 10 - EX using News API.py
@@ -14,12 +14,13 @@ def get_news(topic, from_date, to_date, api_key='057a19ba5af941e2addba5265f0b265
     url = f"https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language=en&apiKey={api_key}"
     r = requests.get(url)
     content = r.json()
+    total = content['totalResults']
     articles = content['articles']
-    rint = rn(0,len(articles))
-    print(rint)
+    rint = rn(0,total-1)
     title = f"TITLE:\n{articles[rint]['title']}\n"
     desc = f"DESCRIPTION:\n{articles[rint]['description']}"
     return title + desc
+
 
 topic = input('Enter your desire topic:\n')
 from_date = input('Enter the starter date in this format(yyyy-mm-dd):\n')
