a3bcc3c3305a060af1dbde630cb1c4935304ca04
Felipe Araujo <zenycsoficial@gmail.com>
2023-07-25 22:39:48 -0300

    two new lessons and now using api to get the news

---
+++ AULA 10 - EX using News API.py
@@ -0,0 +1,31 @@
+import requests
+from random import randint as rn
+
+# content = r.json()
+# articles = content['articles']
+# print(type(articles))
+
+# for x in articles:
+    # print('TITLE:\n',x['title'],
+          # '\nDESCRIPTION:\n',x['description'])
+
+def get_news(topic, from_date, to_date, api_key='057a19ba5af941e2addba5265f0b265e'):
+    """Seta as preferencias na URL e aleatoriaza um topico"""
+    url = f"https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language=en&apiKey={api_key}"
+    r = requests.get(url)
+    content = r.json()
+    articles = content['articles']
+    rint = rn(0,len(articles))
+    print(rint)
+    title = f"TITLE:\n{articles[rint]['title']}\n"
+    desc = f"DESCRIPTION:\n{articles[rint]['description']}"
+    return title + desc
+
+topic = input('Enter your desire topic:\n')
+from_date = input('Enter the starter date in this format(yyyy-mm-dd):\n')
+to_date = input('Enter the end date in this format(yyyy-mm-dd):\n')
+
+if " " in topic:
+    topic.replace(" ", "%20")
+
+print(get_news(topic, from_date, to_date))
---
+++ AULA 11 - Contry Top Headlines.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_news(country, api_key='057a19ba5af941e2addba5265f0b265e'):
+    """Seta as preferencias na URL e aleatoriaza um topico"""
+    url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}"
+    r = requests.get(url)
+    content = r.json()
+    article = content['articles']
+    results = []
+    for article in article:
+        results.append(F"TITLE\n{article['title']}, \nDESCRIPTION: {article['description']}")
+    return results
+
+country = input('Enter your desire Country(us, br, fr, jp...):\n')
+
+print(get_news(country))
