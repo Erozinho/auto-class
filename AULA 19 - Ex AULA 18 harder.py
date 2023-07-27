54f79155c2eece6d8c9d36b1f5ed87bcdb3f0900
Felipe Araujo <zenycsoficial@gmail.com>
2023-07-27 15:38:24 -0300

    New exercise

---
+++ AULA 19 - Ex AULA 18 harder.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+root_dir = Path('automations-lessons/AULA 19')
+file_paths = root_dir.glob('**/*')
+
+for path in file_paths:
+    if path.is_file():
+        parts = path.parts
+        new_name = parts[2] + "-" + parts[3] + "-" + path.name
+        new_filepath = path.with_name(new_name)
+        path.rename(new_filepath)
