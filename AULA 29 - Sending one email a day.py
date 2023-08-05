29264e1f579318cb00afdd0834675bd87fb09647
Felipe Araujo <zenycsoficial@gmail.com>
2023-08-05 19:09:20 -0300

    New exercise

---
+++ AULA 29 - Sending one email a day.py
@@ -0,0 +1,19 @@
+import yagmail
+from time import sleep
+from datetime import datetime as dt
+
+sender = 'felipe9a.cva@gmail.com'
+receiver = 'zenycsoficial@gmail.com'
+
+subject = 'EMAIL TESTE!'
+
+content = """
+EMAIL TESTE FAVOR IGNORAR!
+"""
+while True:
+    now = dt.now()
+    if now.hour == 13:
+        sleep(3600)
+        yag = yagmail.SMTP(user=sender, password=input('Your Password:\n'))
+        yag.send(to=receiver,subject=subject,contents=content)
+        print('Email Sent!')
