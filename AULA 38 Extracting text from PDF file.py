e57371af1f5a75c84cfa226df315cb1b2125aced
Erozinho <zenycsoficial@gmail.com>
2024-02-23 17:05:02 -0300

    a

--- AULA 38 Extracting text from PDF file.py
+++ AULA 38 Extracting text from PDF file.py
@@ -1,5 +1,8 @@
 import fitz
 
 with fitz.open("PDF-TESTE.pdf") as pdf:
+    page1 = pdf[0].get_text()
+    print(page1)
     for page in pdf:
-        print(page.get_text())
+        pass
+        # print(page.get_text())
